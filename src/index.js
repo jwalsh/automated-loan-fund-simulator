89d3e0876cbaf8c2e4750373939df09affd4ad0e
Jason Walsh <j@wal.sh>
2016-09-28 14:56:40 -0400

    Model events

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 // Nomenclature reflects Revolving Loan Funds
 // http://energy.gov/eere/slsc/revolving-loan-funds
 
+var stoch = require('@jwalsh/stochastic');
+
 // Data store
 var ledger = 'ledger/';  // GitHub 
 
@@ -14,20 +16,64 @@ var paypal = process.env.ALE_PAYPAL_KEY
 var paypalSecret = process.env.ALE_PAYPAL_KEY
 
 // s/PRESIDENT/COMPANY
-var events = {
-  COMPANY_ACCOUNT: '',
-  STATE_INCORPORATES: '',
-  // COMPANY_PROMOTE: '',
-  USER_LOAN_APPLICATION: '',
-  COMPANY_LOAN_DECISION: '',
-  COMPANY_LOAN_CONTRACT: '',
-  COMPANY_LOAN_DISPURSE: '', 
-  USER_LOAN_PAYMENT: '',
-  USER_DEFAULT: '',
-  COMPANY_BALANCE: '',
-  COMPANY_RESOLUTION: ''
-}
+var events = [
+  { e: 'COMPANY_INC_REQUEST' },
+  { e: 'STATE_INC_ACCEPT' },
+  { e: 'STATE_INC_REJECT' },
+  { e: 'END' },
+  { e: 'COMPANY_ACCOUNT_FUNDS_ADD' },
+  { e: 'COMPANY_ACCOUNT_FUNDS_WD' },
+  { e: 'USER_LOAN_APPLICATION' },
+  { e: 'COMPANY_LOAN_DECISION' },
+  { e: 'COMPANY_LOAN_CONTRACT' },
+  { e: 'COMPANY_LOAN_DISPURSE' }, 
+  { e: 'USER_LOAN_PAYMENT' },
+  { e: 'USER_LOAN_BALANCE' },
+  { e: 'USER_LOAN_DEFAULT' },
+  { e: 'COMPANY_BALANCE' },
+  { e: 'UNKNOWN' },
+  { e: 'UNKNOWN' },
+  { e: 'UNKNOWN' },
+  { e: 'UNKNOWN' },
+  { e: 'UNKNOWN' },
+  { e: 'UNKNOWN' }
+];
 console.log(events);
+
+
+// Discrete Time Markof Chain of transition events (unbalanced):
+
+var transMtrx = [
+  [.1, .5, .1, .1, .1, .1, .1, .1, .5, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .5, .1, .1, .1, .1, .1, .5, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .5, .1, .1, .1, .1, .5, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.2, .1, .1, .1, .5, .1, .1, .1, .5, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ], // END
+  [.1, .1, .1, .1, .1, .5, .1, .1, .5, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .5, .1, .5, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .5, .5, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .2, .2, .2, .5, .2, .2, .2, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .2, .2, .2, .2, .5, .2, .2, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .2, .2, .2, .2, .2, .5, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .5, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .5, .1, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .5, .1, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .5, .1, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .5, .1, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .5, .1, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .5, .1, 0 ],
+  [.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [1., .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
+  [.0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, 0 ]
+];
+console.log('NxN transMtrx', transMtrx.length, transMtrx[0].length);
+
+
+var CTMC = stoch.CTMC(transMtrx, 20, 0, true);
+// console.log(CTMC);
+var ctmcEvents = Object.keys(CTMC).map((e, i, c) => {
+  return events[CTMC[e]].e;
+});
+console.log(ctmcEvents);
 
 var forms = {
   LOAN_APPLICATION: {
