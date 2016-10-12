609598b978be2d0d430175bd086cfecc50cf6a2d
Jason Walsh <j@wal.sh>
2016-10-11 20:41:25 -0400

    Model phones, debt, and employment

--- src/index.js
+++ src/index.js
@@ -4,16 +4,56 @@
 // http://energy.gov/eere/slsc/revolving-loan-funds
 
 var stoch = require('@jwalsh/stochastic');
+var faker = require('faker');
 
 // Data store
-var ledger = 'ledger/';  // GitHub 
+var ledger = 'ledger/';  // GitHub
 
-// Model and Configuration 
+// Model and Configuration
 var company = process.env.ALE_COMPANY;
 var account = process.env.ALE_LOAN_ACCESS_KEY;
 var accountSecret = process.env.ALE_LOAN_ACCESS_SECRET;
-var paypal = process.env.ALE_PAYPAL_KEY
-var paypalSecret = process.env.ALE_PAYPAL_KEY
+var paypal = process.env.ALE_PAYPAL_KEY;
+var paypalSecret = process.env.ALE_PAYPAL_KEY;
+
+var User = function() {
+  this.name = faker.name.findName();
+  this.creditScore = Math.ceil(Math.random() * 100);
+  // this.created = (new Date()).getTime();
+  this.verifiedEmployment = Math.random() > .8 ? true : false;
+  this.contractedCell = Math.random() > .2 ? true : false;
+  this.existingDebt = Math.floor(Math.random() * 10000);
+  // console.log(this);
+};
+
+
+var loanRates = function(creditScore) {
+  return 5 +  12 * (100 - parseInt(creditScore)) / 100;
+};
+
+var scoreCredit = function(user) {
+  console.log('scoring credit', user.creditScore);
+  var MIN_SCORE = 20;
+  // move to 0
+  var marketImpact = parseInt((new Date()).getTime().toString().split('').pop());
+  // var loanRate = loanRates(user.creditScore);
+  var employmentImpact = !user.verifiedEmployment ? 10 : 0;
+  var scoreMin = MIN_SCORE + marketImpact + employmentImpact;
+  console.log('MIN_SCORE', MIN_SCORE,  'marketImpact', marketImpact, 'employmentImpact', employmentImpact);
+  var rate = loanRates(user.creditScore);
+  if (user.creditScore > scoreMin) {
+    console.log('accept loan', 'score', user.creditScore, 'at ', rate, scoreMin);
+  } else {
+    console.log('reject loan', 'score', user.creditScore, scoreMin);
+  }
+};
+
+var users = [];
+for (var i = 0; i < 10; i++) {
+  var user = new User();
+  console.log(user);
+  scoreCredit(user);
+}
 
 // s/PRESIDENT/COMPANY
 var events = [
@@ -26,7 +66,7 @@ var events = [
   { e: 'USER_LOAN_APPLICATION' },
   { e: 'COMPANY_LOAN_DECISION' },
   { e: 'COMPANY_LOAN_CONTRACT' },
-  { e: 'COMPANY_LOAN_DISPURSE' }, 
+  { e: 'COMPANY_LOAN_DISPURSE' },
   { e: 'USER_LOAN_PAYMENT' },
   { e: 'USER_LOAN_BALANCE' },
   { e: 'USER_LOAN_DEFAULT' },
@@ -38,7 +78,7 @@ var events = [
   { e: 'UNKNOWN' },
   { e: 'UNKNOWN' }
 ];
-console.log(events);
+// console.log(events);
 
 
 // Discrete Time Markof Chain of transition events (unbalanced):
@@ -65,7 +105,7 @@ var transMtrx = [
   [1., .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, 0 ],
   [.0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, 0 ]
 ];
-console.log('NxN transMtrx', transMtrx.length, transMtrx[0].length);
+// console.log('NxN transMtrx', transMtrx.length, transMtrx[0].length);
 
 
 var CTMC = stoch.CTMC(transMtrx, 20, 0, true);
@@ -73,7 +113,7 @@ var CTMC = stoch.CTMC(transMtrx, 20, 0, true);
 var ctmcEvents = Object.keys(CTMC).map((e, i, c) => {
   return events[CTMC[e]].e;
 });
-console.log(ctmcEvents);
+// console.log(ctmcEvents);
 
 var forms = {
   LOAN_APPLICATION: {
@@ -83,14 +123,13 @@ var forms = {
   },
   LOAN: {
     amount: new Number(),
-    term_days: new Number() 
+    term_days: new Number()
   }
 }
-console.log(forms);
+// console.log(forms);
 
 var contract = {
   LOAN_APPLICATION:  function(template) { return template.name === 'Sally Smith' && template.amount < 5.5; },
   LOAN_REPAYMENT: function(template) { return }
 }
-console.log(contract);
-
+// console.log(contract);
