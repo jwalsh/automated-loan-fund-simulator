5d646f2ce56517c9419b0399e5a38c2233130dbd
Jason Walsh <j@wal.sh>
2016-11-03 20:30:22 -0400

    GET Payment
    https://developer.paypal.com/docs/integration/web/accept-paypal-payment/

--- src/paypal-demo.js
+++ src/paypal-demo.js
@@ -8,3 +8,17 @@ paypal.configure({
 });
 
 console.log(paypal);
+
+
+var paymentId = "PAY-0XL713371A312273YKE2GCNI";
+
+paypal.payment.get(paymentId, function (error, payment) {
+    if (error) {
+        console.log(error);
+        throw error;
+    } else {
+        console.log("Get Payment Response");
+        console.log(JSON.stringify(payment));
+    }
+
+});
